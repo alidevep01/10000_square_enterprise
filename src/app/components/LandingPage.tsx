2da49cb0fec5094ed7c87bdb36ba422afa390b02
Sairam Koripelly <skkoripe@amazon.com>
2024-10-04 18:15:45 +0530

    Added How it works modal

---
+++ src/app/components/LandingPage.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, {useState} from "react";
+import Grid from "@/app/components/Grid";
+import HowItWorksModal from "@/app/components/HowItWorksModal";
+
+export default function LandingPage() {
+    const [isHTWModalOpen, setIsHTWModalOpen] = useState(false);
+
+    const openHTWModal = () => setIsHTWModalOpen(true);
+    const closeHTWModal = () => setIsHTWModalOpen(false);
+
+    const squareCount = 10000; // Set the desired number of squares to display
+    const squareSize = 40;
+    const gap = 3;
+
+    const winnerSquareCount = squareCount / 500; // Set the desired number of squares to display
+    const winnerSquareSize = 60;
+    const winnerSquareGap = 3;
+
+    const marginX = 15;
+
+    return (
+        <div className="flex flex-col gap-20 w-screen overflow-x-hidden">
+            <div className="mx-2">
+                <div className="mt-4 flex justify-between items-center">
+                    <h1 className="font-bold text-3xl mb-1">Welcome to Mystery Square Club</h1>
+                    <button
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+                        onClick={openHTWModal}
+                    >
+                        How it works?
+                    </button>
+                </div>
+                <p>Get more than just renting a square!</p>
+            </div>
+
+            {/* Render the Grid with the specified count */}
+            <Grid count={winnerSquareCount} squareSize={winnerSquareSize} gap={winnerSquareGap} marginX={marginX}/>
+            <Grid count={squareCount} squareSize={squareSize} gap={gap} marginX={marginX}/>
+
+            {/* Render the HowItWorks Modal */}
+            <HowItWorksModal isOpen={isHTWModalOpen} onClose={closeHTWModal}/>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,11 @@
 // src/app/page.tsx
 
 import React from "react";
-import Grid from "@/app/components/Grid";
+import LandingPage from "@/app/components/LandingPage";
 
 export default function Home() {
-    const squareCount = 10000; // Set the desired number of squares to display
-    const squareSize = 40;
-    const gap = 3;
-
-    const winnerSquareCount = squareCount / 500; // Set the desired number of squares to display
-    const winnerSquareSize = 60;
-    const winnerSquareGap = 3;
-
-    const marginX = 15;
 
     return (
-        <div className="flex flex-col gap-20 w-screen overflow-x-hidden">
-            <div className="mx-2">
-                <div className="mt-4 flex justify-between items-center">
-                    <h1 className="font-bold text-3xl mb-1">Welcome to Mystery Square Club</h1>
-                    <button
-                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
-                        Rules
-                    </button>
-                </div>
-                <p>Get more than just renting a square!</p>
-            </div>
-
-            {/* Render the Grid with the specified count */}
-            <Grid count={winnerSquareCount} squareSize={winnerSquareSize} gap={winnerSquareGap} marginX={marginX}/>
-            <Grid count={squareCount} squareSize={squareSize} gap={gap} marginX={marginX}/>
-        </div>
+       <LandingPage></LandingPage>
     );
 }
